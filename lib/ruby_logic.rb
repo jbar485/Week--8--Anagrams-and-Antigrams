4d8261a5eefe943ffaa67345bcafd4257be0c836
Judah Barton <judah.barton@yahoo.com>
2019-12-13 14:00:03 -0800

    add loops to work with sentences. This is just the beginning

--- lib/ruby_logic.rb
+++ lib/ruby_logic.rb
@@ -1,34 +1,79 @@
-class Word
+class Anagram
   attr_reader(:word_input1, :word_input2)
   def initialize(word_input1, word_input2)
     @word_input1 = word_input1.downcase()
     @word_input2 = word_input2.downcase()
   end
 
+
+
   def word_test
-    word1 = @word_input1.chars.sort.join
-    word2 = @word_input2.chars.sort.join
-    if word1 =~ /[aeiou]/ && word2 =~ /[aeiou]/
-      if word1 === word2
-        final_answer = "These words are anagrams."
-      else
-        word11 = word1.split("")
-        word22 = word2.split("")
-        antigram_counter = []
-        word11.each do |letter1|
-          if word22.include?(letter1)
-            antigram_counter.push("BAD")
+    word1_array = @word_input1.split(" ")
+    word2_array = @word_input2.split(" ")
+    final = []
+    if word1_array.length() == word2_array.length()
+      word1_array.each do |array_one|
+        word2_array.each do |array_two|
+          word1 = array_one.chars.sort.join
+          word2 = array_two.chars.sort.join
+          if word1 =~ /[aeiou]/ && word2 =~ /[aeiou]/
+            if word1 === word2
+              final.push("These words are anagrams.")
+            else
+              word11 = word1.split("")
+              word22 = word2.split("")
+              antigram_counter = []
+              word11.each do |letter1|
+                if word22.include?(letter1)
+                  antigram_counter.push("BAD")
+                end
+              end
+              if antigram_counter.include?("BAD")
+                final.push("You have two words that are neither anagrams or antigrams")
+              else
+                final.push("These words have no letter matches and are antigrams.")
+              end
+            end
+          else
+            final.push("You need to input actual words")
           end
-        end
-        if antigram_counter.include?("BAD")
-          final_answer = "You have two words that are neither anagrams or antigrams"
-        else
-          final_answer = "These words have no letter matches and are antigrams."
         end
       end
     else
-      final_answer = "You need to input actual words"
+      final.push("Enter the same amount of words per sentence.")
     end
+    final_answer = final.join
     final_answer
   end
+
+
+
+
+
+  # def word_test
+  #   word1 = @word_input1.chars.sort.join
+  #   word2 = @word_input2.chars.sort.join
+  #   if word1 =~ /[aeiou]/ && word2 =~ /[aeiou]/
+  #     if word1 === word2
+  #       final_answer = "These words are anagrams."
+  #     else
+  #       word11 = word1.split("")
+  #       word22 = word2.split("")
+  #       antigram_counter = []
+  #       word11.each do |letter1|
+  #         if word22.include?(letter1)
+  #           antigram_counter.push("BAD")
+  #         end
+  #       end
+  #       if antigram_counter.include?("BAD")
+  #         final_answer = "You have two words that are neither anagrams or antigrams"
+  #       else
+  #         final_answer = "These words have no letter matches and are antigrams."
+  #       end
+  #     end
+  #   else
+  #     final_answer = "You need to input actual words"
+  #   end
+  #   final_answer
+  # end
 end
--- spec/ruby_logic_spec.rb
+++ spec/ruby_logic_spec.rb
@@ -2,30 +2,30 @@ require ('rspec')
 require ('pry')
 require ('ruby_logic')
 
-describe('Word#word_test') do
+describe('Anagram#word_test') do
 
   it('will return a message telling user it is an anagram') do
-    anagram = Word.new("ruby", "bury")
+    anagram = Anagram.new("ruby", "bury")
     expect(anagram.word_test).to(eq("These words are anagrams."))
   end
 
   it('will return a message telling user it is an anagram no matter the casing') do
-    anagram = Word.new("Ruby", "bUry")
+    anagram = Anagram.new("Ruby", "bUry")
     expect(anagram.word_test).to(eq("These words are anagrams."))
   end
 
   it('will return a message telling user they need to enter a real word') do
-    vowel_finder = Word.new("rurb", "brrp")
+    vowel_finder = Anagram.new("rurb", "brrp")
     expect(vowel_finder.word_test).to(eq("You need to input actual words"))
   end
 
   it('will return a message telling the user it is not an anagram but a antigram') do
-    antigram = Word.new("abcd", "efgh")
+    antigram = Anagram.new("abcd", "efgh")
     expect(antigram.word_test).to(eq("These words have no letter matches and are antigrams."))
   end
 
   it('will return a message telling the user it is not an anagram or a antigram') do
-    nothing = Word.new("cool", "pool")
+    nothing = Anagram.new("cool", "pool")
     expect(nothing.word_test).to(eq("You have two words that are neither anagrams or antigrams"))
   end
 
